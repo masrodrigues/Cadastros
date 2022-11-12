1d266865464330fca8a8a3493602de15c3824456
Marco Rodrigues <masrodrigues@inf.ufrgs.br>
2022-11-12 00:42:56 -0300

    feat: Adicionado React-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-
-
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./Routes";
 export const App = () =>{
   return (
-    <div className="App">
-     Teste
-    </div>
+    <BrowserRouter>
+    <AppRoutes />
+    </BrowserRouter>
   );
 }
 
