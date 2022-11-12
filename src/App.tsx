32cca39dcb3ad3e3ca6ed1cfaf4eaddc3655b756
Marco Rodrigues <masrodrigues@inf.ufrgs.br>
2022-11-12 01:42:37 -0300

    feat: Adicionado tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./Routes";
+import { LightTheme } from "./Shared/themes";
 export const App = () =>{
   return (
-    <BrowserRouter>
-    <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
